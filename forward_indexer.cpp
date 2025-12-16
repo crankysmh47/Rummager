3b61268eac1432c5140c728246740f9635411491
ahmed-rehan-34 <ahmedrehantauseef@gmail.com>
2025-12-17 00:48:06 +0500

    Modified it to produce document lengths .bin file

--- forward_indexer.cpp
+++ forward_indexer.cpp
@@ -5,11 +5,10 @@
 #include <unordered_map>
 #include <map>
 #include <cstdint>
-#include "common.h" // <--- INCLUDES STOPWORDS & TOKENIZER
+#include "common.h" // Includes STOPWORDS & TOKENIZER
 
 using namespace std;
 
-// This Lexicon class is for LOADING (Reading), not building
 class LexiconLoader {
 private:
     unordered_map<string, int> wordToID;
@@ -27,7 +26,7 @@ public:
             inFile.read((char*)&len, sizeof(len));
             string word(len, ' ');
             inFile.read(&word[0], len);
-            wordToID[word] = i; 
+            wordToID[word] = i;
         }
         return true;
     }
@@ -36,7 +35,7 @@ public:
         auto it = wordToID.find(word);
         return (it != wordToID.end()) ? it->second : -1;
     }
-    
+
     size_t size() const { return wordToID.size(); }
 };
 
@@ -47,63 +46,72 @@ int main() {
         return 1;
     }
 
-    ifstream infile("clean_dataset.txt"); 
+    ifstream infile("clean_dataset.txt");
     ofstream outfile("forward_index.bin", ios::binary);
-    ofstream mapfile("docid_map.txt");
+    ofstream lenFile("doc_lengths.bin", ios::binary);
 
-    if (!infile || !outfile || !mapfile) {
-        cerr << "File error." << endl; 
+    if (!infile.is_open() || !outfile.is_open() || !lenFile.is_open()) {
+        cerr << "Error opening file(s)!" << endl;
         return 1;
     }
 
+    // Placeholder for total docs; will rewrite later
+    uint32_t totalDocs = 0;
+    lenFile.write((char*)&totalDocs, sizeof(totalDocs));
+
     string line;
     uint32_t internalDocID = 0;
-    map<int, int> docWordFreq; 
-
-    cout << "Generating Forward Index..." << endl;
 
     while (getline(infile, line)) {
         size_t tabPos = line.find('\t');
         if (tabPos == string::npos) continue;
 
-        string originalDocID = line.substr(0, tabPos);
         string content = line.substr(tabPos + 1);
-
-        mapfile << internalDocID << " " << originalDocID << "\n";
-
-        // USES SHARED TOKENIZER (Guaranteed same logic as builder)
         vector<string> tokens = Tokenize::tokenize(content);
-        
-        docWordFreq.clear();
-        int totalWordsInDoc = 0;
+
+        map<int, int> docWordFreq;
+        uint32_t totalWordsInDoc = 0;
 
         for (const string& token : tokens) {
-            int id = lexicon.getID(token);
-            if (id != -1) {
-                docWordFreq[id]++;
+            int wordID = lexicon.getID(token);
+            if (wordID != -1) {
+                docWordFreq[wordID]++;
                 totalWordsInDoc++;
             }
         }
 
         uint32_t uDocID = internalDocID;
-        uint32_t uTotal = (uint32_t)totalWordsInDoc;
-        uint32_t uUnique = (uint32_t)docWordFreq.size();
+        uint32_t uTotal = totalWordsInDoc;
+        uint32_t uUnique = docWordFreq.size();
 
+        // Write forward index
         outfile.write((char*)&uDocID, sizeof(uDocID));
         outfile.write((char*)&uTotal, sizeof(uTotal));
         outfile.write((char*)&uUnique, sizeof(uUnique));
 
         for (const auto& pair : docWordFreq) {
-            uint32_t uWordID = (uint32_t)pair.first;
-            uint32_t uFreq = (uint32_t)pair.second;
+            uint32_t uWordID = pair.first;
+            uint32_t uFreq = pair.second;
             outfile.write((char*)&uWordID, sizeof(uWordID));
             outfile.write((char*)&uFreq, sizeof(uFreq));
         }
 
+        // Write doc length
+        lenFile.write((char*)&uTotal, sizeof(uTotal));
+
         internalDocID++;
-        if (internalDocID % 10000 == 0) cout << "Indexed " << internalDocID << "...\r" << flush;
+        if (internalDocID % 10000 == 0)
+            cout << "Indexed " << internalDocID << " documents...\r" << flush;
     }
 
-    cout << "\nDone!" << endl;
+    // Rewrite totalDocs at start of lengths file
+    lenFile.seekp(0);
+    lenFile.write((char*)&internalDocID, sizeof(internalDocID));
+
+    infile.close();
+    outfile.close();
+    lenFile.close();
+
+    cout << "\nDone! Total documents indexed: " << internalDocID << endl;
     return 0;
 }
